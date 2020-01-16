84f607eb61d0cec47daaef08f8e36a29b6fb3f1f
Matthieu Harbonnier <matthieu.harbonnier@gmail.com>
2020-01-16 18:18:24 +0100

    Display teams index, handle team creating, heroku setup

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,20 @@
 class TeamsController < ApplicationController
 	def index
-		
+		@teams = Team.all		
+	end
+
+	def new
+		@team = Team.new
+	end
+
+	def create
+		Team.create(team_params)
+		redirect_to teams_path
+	end
+
+	private
+
+	def team_params
+		params.require(:team).permit(:name, :acronym, :city, :light_color)
 	end
 end
