4842ff98832e2813a9ab70674ee0f9ca699d4c9c
Matthieu Harbonnier <matthieu.harbonnier@gmail.com>
2020-01-22 20:39:26 +0100

    Add stage in game creation form + add menu in layout

--- app/controllers/fixtures_controller.rb
+++ app/controllers/fixtures_controller.rb
@@ -5,6 +5,13 @@ class FixturesController < ApplicationController
 
 	def new
 		@fixture = Fixture.new
+		i=1
+		@stages =[]
+
+		while i<=38
+			@stages << i
+			i=i+1
+		end
 		
 		teams_for_array = Team.all.order(city: :asc)
 		@teams = teams_for_array.map do |d|
@@ -28,7 +35,8 @@ class FixturesController < ApplicationController
 		
 		home_t = Team.find(params[:home_team].to_i)
 		away_t = Team.find(params[:away_team].to_i)
-		Fixture.create(date:date,time:time,home_team:home_t,away_team:away_t,status:status)
+		stage = params[:stage]
+		Fixture.create(date:date,time:time,home_team:home_t,away_team:away_t,status:status,stage:stage)
 
 		redirect_to fixtures_path
 	end
@@ -58,6 +66,6 @@ class FixturesController < ApplicationController
 	private
 
 	def fixture_params
-		params.require(:fixture).permit(:date, :time, :home_team, :away_team, :scorehome, :scoreaway, :status)
+		params.require(:fixture).permit(:date, :time, :home_team, :away_team, :scorehome, :scoreaway, :status, :stage)
 	end
 end
