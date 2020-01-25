c1c692f401c3e2bd11a09c7cc12cbefa804ce443
Matthieu Harbonnier <matthieu.harbonnier@gmail.com>
2020-01-25 23:30:29 +0100

    Apply nex rank calculation after fixture update

--- app/controllers/fixtures_controller.rb
+++ app/controllers/fixtures_controller.rb
@@ -58,10 +58,33 @@ class FixturesController < ApplicationController
 	end
 
 	def update
+		################
+		# Fixture update
+		################
 		@fixturetoupdate = Fixture.find(params[:id])
 		@fixturetoupdate.completed = true
 		@fixturetoupdate.status = "played" # /!\ Should be removed when status auto updated by CRON task
 		@fixturetoupdate.update(fixture_params)
+		################
+		# Ranking update
+		################
+		homerank = Rank.where(team:@fixturetoupdate.home_team).last.level
+		awayrank = Rank.where(team:@fixturetoupdate.away_team).last.level
+		homescore = params[:fixture][:scorehome]
+		awayscore = params[:fixture][:scoreaway]
+		# ---- Home team
+		newhomerank = rank_calculation(homerank,
+			k_factor(homerank),
+			game_result(homescore,awayscore),
+			win_rate(homerank,awayrank))
+		Rank.create(fixture:@fixturetoupdate,team:@fixturetoupdate.home_team,level:newhomerank)
+		# ---- Away team
+		newawayrank = rank_calculation(awayrank,
+			k_factor(awayrank),
+			game_result(awayscore,homescore),
+			win_rate(awayrank,homerank))
+		Rank.create(fixture:@fixturetoupdate,team:@fixturetoupdate.away_team,level:newawayrank)
+		################
 		redirect_to to_complete_fixtures_path
 	end
 
@@ -72,5 +95,46 @@ class FixturesController < ApplicationController
 
 	def fixture_params
 		params.require(:fixture).permit(:date, :time, :home_team, :away_team, :scorehome, :scoreaway, :status, :stage)
+	end
+
+	def k_factor(ranking)
+		case
+		when ranking < 1800
+			kfactor=80
+		when (1800..1899) === ranking
+			kfactor=70
+		when (1900..1999) === ranking
+			kfactor=60
+		when (2000..2099) === ranking
+			kfactor=50
+		when (2100..2199) === ranking
+			kfactor=40
+		when (2200..2299) === ranking
+			kfactor=30
+		when ranking >= 2300
+			kfactor=20
+		end
+		return kfactor
+	end
+
+	def win_rate(p1ranking,p2ranking)
+		winrate = 1/(1+10**((p2ranking.to_f-p1ranking.to_f)/400))
+		return winrate
+	end
+
+	def game_result(team_score, opponent_score)
+		case 
+		when team_score < opponent_score
+			gameresult = 0
+		when team_score == opponent_score
+			gameresult = 0.5
+		when team_score > opponent_score
+			gameresult = 1
+		end
+	end
+
+	def rank_calculation(prev_rank, k_factor, game_result, win_rate)
+		new_rank = prev_rank + k_factor*(game_result - win_rate)
+		return new_rank.round(0)
 	end
 end
--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,6 +1,17 @@
 class RanksController < ApplicationController
 	def index
 		@ranks = Rank.all
+
+		ranking = []
+		teams = Team.all
+		teams.each do |targetteam|
+			tempteam = []
+			theteam = Rank.where(team:targetteam).last
+			tempteam << theteam.team.name
+			tempteam << theteam.level
+			ranking << tempteam
+		end
+		@theranking = ranking.sort { |a,b| b[1] <=> a[1] }
 	end
 
 	def initializeranking
