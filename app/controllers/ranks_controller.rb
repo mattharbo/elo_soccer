9fffb764cf36bc4cb86e87b16438a30975f48072
Matthieu Harbonnier <matthieu.harbonnier@gmail.com>
2020-01-27 21:50:44 +0100

    Remove Team initializer

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,25 +1,23 @@
 class RanksController < ApplicationController
 	def index
-		# @ranks = Rank.all
+		@ranks = Rank.all
 
-		# ranking = []
-		# teams = Team.all
-		# teams.each do |targetteam|
-		# 	tempteam = []
-		# 	theteam = Rank.where(team:targetteam).last
-		# 	tempteam << theteam.team.name
-		# 	tempteam << theteam.level
-		# 	ranking << tempteam
-		# end
-		# @theranking = ranking.sort { |a,b| b[1] <=> a[1] }
+		ranking = []
+		teams = Team.all
+		teams.each do |targetteam|
+			tempteam = []
+			theteam = Rank.where(team:targetteam).last
+			tempteam << theteam.team.name
+			tempteam << theteam.level
+			ranking << tempteam
+		end
+		@theranking = ranking.sort { |a,b| b[1] <=> a[1] }
 	end
 
 	def initializeranking
-		@teams = Team.all.order(level: :desc)
+		teams = Team.all.order(level: :desc)
 		defaultlevel = 2000
-		@teams.each do |team|
-			Rank.create(team:team,level:defaultlevel)
-		end
+		teams.each { |team| Rank.create(team:team,level:defaultlevel) }
 		redirect_to ranks_path
 	end
 end
