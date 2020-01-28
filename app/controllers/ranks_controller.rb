1ff1e0601f6faee0e6221389756bcce7a09e026f
Matthieu Harbonnier <matthieu.harbonnier@gmail.com>
2020-01-28 22:55:24 +0100

    Game seed fix

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -13,11 +13,25 @@ class RanksController < ApplicationController
 		end
 		@theranking = ranking.sort { |a,b| b[1] <=> a[1] }
 	end
-
-	def initializeranking
-		teams = Team.all.order(level: :desc)
-		defaultlevel = 2000
-		teams.each { |team| Rank.create(team:team,level:defaultlevel) }
-		redirect_to ranks_path
-	end
 end
+
+# Rank.create(team:Team.find(1),level:2000)
+# Rank.create(team:Team.find(2),level:2000)
+# Rank.create(team:Team.find(3),level:2000)
+# Rank.create(team:Team.find(4),level:2000)
+# Rank.create(team:Team.find(5),level:2000)
+# Rank.create(team:Team.find(6),level:2000)
+# Rank.create(team:Team.find(7),level:2000)
+# Rank.create(team:Team.find(8),level:2000)
+# Rank.create(team:Team.find(9),level:2000)
+# Rank.create(team:Team.find(10),level:2000)
+# Rank.create(team:Team.find(11),level:2000)
+# Rank.create(team:Team.find(12),level:2000)
+# Rank.create(team:Team.find(13),level:2000)
+# Rank.create(team:Team.find(14),level:2000)
+# Rank.create(team:Team.find(15),level:2000)
+# Rank.create(team:Team.find(16),level:2000)
+# Rank.create(team:Team.find(17),level:2000)
+# Rank.create(team:Team.find(18),level:2000)
+# Rank.create(team:Team.find(19),level:2000)
+# Rank.create(team:Team.find(20),level:2000)
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,50 +11,92 @@
 ############################ Feed the DB with L1 legacy games ##################################
 ################################################################################################
 
-# require 'net/http'
-# require 'json'
+require 'net/http'
+require 'json'
 
-# url = 'https://spreadsheets.google.com/feeds/cells/1mcsw796p1dt9C4njlvOu9Xuvgn4Ztmc2SZonopawBSs/4/public/full?alt=json'
-# uri = URI(url)
-# response = Net::HTTP.get(uri)
-# fixture_hash = JSON.parse(response)
+url = 'https://spreadsheets.google.com/feeds/cells/1mcsw796p1dt9C4njlvOu9Xuvgn4Ztmc2SZonopawBSs/4/public/full?alt=json'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+fixture_hash = JSON.parse(response)
 
-# x = 1
-# y = 1
+allcells = fixture_hash["feed"]["entry"]
+$x = 0
+$temp = []
 
-# while (x > 0) && (x <= 2000)
-# 	if (fixture_hash["feed"]["entry"][x]["gs$cell"]["$t"] != "date") && (fixture_hash["feed"]["entry"][x]["gs$cell"]["$t"] != "time") && (fixture_hash["feed"]["entry"][x]["gs$cell"]["$t"] != "stage") && (fixture_hash["feed"]["entry"][x]["gs$cell"]["$t"] != "home_team") && (fixture_hash["feed"]["entry"][x]["gs$cell"]["$t"] != "away_team") && (fixture_hash["feed"]["entry"][x]["gs$cell"]["$t"] != "scorehome") && (fixture_hash["feed"]["entry"][x]["gs$cell"]["$t"] != "scoreaway") && (fixture_hash["feed"]["entry"][x]["gs$cell"]["$t"] != "status")
+allcells.each do |acell|
 
-# 	  case fixture_hash["feed"]["entry"][x]["gs$cell"]["col"]
+	if (acell["gs$cell"]["row"].to_i != 1)
+
+	$x += 1
+
+	  case acell["gs$cell"]["col"].to_i
+	  when 1
+	    $temp << acell["gs$cell"]["$t"]
+	  when 2
+	    $temp << acell["gs$cell"]["$t"]
+	  when 3
+	    $temp << acell["gs$cell"]["$t"].to_i
+	  when 4
+	    $temp << Team.where(city:acell["gs$cell"]["$t"]).take
+	  when 5
+	    $temp << Team.where(city:acell["gs$cell"]["$t"]).take
+	  when 6
+	    $temp << acell["gs$cell"]["$t"]
+	  when 7
+	    $temp << acell["gs$cell"]["$t"]
+	  when 8
+	    $temp << acell["gs$cell"]["$t"]
+	  end
+
+		puts "Done for cell #{$x}"
+
+		if ($x % 8 == 0)
+			Fixture.create(date:$temp[0],time:$temp[1],home_team:$temp[3],away_team:$temp[4],status:$temp[7],stage:$temp[2],scorehome:$temp[5],scoreaway:$temp[6])
+			puts "Game created"
+			$temp.clear
+		end
+			
+	end
+end
+
+
+# cell = 0
+# rawcomplete = 0
+
+# while (cell <= fixture_hash["feed"]["entry"].count)
+# 	if (fixture_hash["feed"]["entry"][cell]["gs$cell"]["row"].to_i != 1)
+
+# 	  case fixture_hash["feed"]["entry"][cell]["gs$cell"]["col"]
 # 	  when "1"
-# 	    date=fixture_hash["feed"]["entry"][x]["gs$cell"]["$t"]
+# 	    date=fixture_hash["feed"]["entry"][cell]["gs$cell"]["$t"]
 # 	  when "2"
-# 	    time=fixture_hash["feed"]["entry"][x]["gs$cell"]["$t"]
+# 	    time=fixture_hash["feed"]["entry"][cell]["gs$cell"]["$t"]
 # 	  when "3"
-# 	    stage=fixture_hash["feed"]["entry"][x]["gs$cell"]["$t"].to_i
+# 	    stage=fixture_hash["feed"]["entry"][cell]["gs$cell"]["$t"].to_i
 # 	  when "4"
-# 	    home_team=Team.where(city:fixture_hash["feed"]["entry"][x]["gs$cell"]["$t"]).take
+# 	    home_team=Team.where(city:fixture_hash["feed"]["entry"][cell]["gs$cell"]["$t"]).take
 # 	  when "5"
-# 	    away_team=Team.where(city:fixture_hash["feed"]["entry"][x]["gs$cell"]["$t"]).take
+# 	    away_team=Team.where(city:fixture_hash["feed"]["entry"][cell]["gs$cell"]["$t"]).take
 # 	  when "6"
-# 	    scorehome=fixture_hash["feed"]["entry"][x]["gs$cell"]["$t"]
+# 	    scorehome=fixture_hash["feed"]["entry"][cell]["gs$cell"]["$t"]
 # 	  when "7"
-# 	    scoreaway=fixture_hash["feed"]["entry"][x]["gs$cell"]["$t"]
+# 	    scoreaway=fixture_hash["feed"]["entry"][cell]["gs$cell"]["$t"]
 # 	  when "8"
-# 	    status=fixture_hash["feed"]["entry"][x]["gs$cell"]["$t"]
-# 	  end
-
-# 	  if (y == 8)
-# 	  	Fixture.create(date:date,time:time,home_team:home_team,away_team:away_team,status:status,stage:stage,scorehome:scorehome,scoreaway:scoreaway)
-# 	  	puts "New game created!"
-# 	  	y = 1
-# 	  else
-# 	  	y += 1
+# 	    status=fixture_hash["feed"]["entry"][cell]["gs$cell"]["$t"]
 # 	  end
 
 # 	end
-# 	puts "Done for #{x}"
-# 	x += 1
+
+# 	  if rawcomplete == 8
+# 	  	Fixture.create(date:date,time:time,home_team:home_team,away_team:away_team,status:status,stage:stage,scorehome:scorehome,scoreaway:scoreaway)
+# 	  	puts "New game created!"
+# 	  	rawcomplete = 1
+# 	  else
+# 	  	rawcomplete += 1
+# 	  end
+
+# 	puts "Done for #{cell}"
+# 	cell += 1
 # end
 
 ################################################################################################
